11ce639132312a79dbd10912d96c94772d5dbdb8
itstemmy <temmymamasworld@gmail.com>
2023-01-08 17:10:43 -0800

    adding new python file

---
+++ variable.py
@@ -0,0 +1,33 @@
+fullName = "Sulaiman Muhammed"
+# Text type
+# 1 String 
+# Numeric Type
+# 1 integer
+# 2 Float
+# complex
+# Sequence 
+# 1 List
+# 2 Tupple
+# 3 range
+# Set Type
+# 1 set
+# boolean type
+# 1 boolean
+
+
+text =  "My name is Sulaiman Muhammed"
+#index of a string starts from zero 
+#len(variable) this returns the length of a string
+#print(len(text))
+#print(text[-1])
+#slicing of a string with positive indexing
+# variable[starting index : stoping indext]
+#example
+course = ["MATH","ENGLISH","PLB",'PHY',"CHE","ZLY"]
+
+for i in range(len(course)):
+    if i%2==0:
+       print(course[i])
+
+
+
